87060bcad8dbfa2d4b04cbce655b317364fc71c2
= <=>
2016-10-31 13:48:30 -0400

    Dictionaries in color_triangles changed to simply maps Since they are all based on each other, they will all be in the same order This brings a minor speed increase

--- python/lowPoly.py
+++ python/lowPoly.py
@@ -6,56 +6,19 @@ from scipy.spatial import Delaunay
 
 
 # create a new image using the average color for each triangle
-def getLowPoly(tris, im):
+def color_triangles(tris, im):
 
-    # both triangle_vertices and bounding rectangles "share" the first element
-    # that is, both refer to the triangle index, for triangle_vertices this is the triangle that it is,
-    # for bounding_rectangles this is the triangle that the rectangle bounds
+    triangle_vertices = map(lambda vertices: np.array([vertices]), tris.points[tris.vertices].astype(np.int32))
 
-    triangle_vertices = {triangle_id: np.array([tris.points[vertices]]).astype(np.int32)
-                         for triangle_id, vertices in enumerate(tris.vertices)}
+    bounding_rectangles = map(lambda vertices: cv2.boundingRect(vertices), triangle_vertices)
 
-    bounding_rectangles = {triangle_id: cv2.boundingRect(vertices)
-                           for triangle_id, vertices in triangle_vertices.iteritems()}
-
-    triangle_means = {triangle_id: cv2.mean(im[r:(r + h), c:(c + w), :])
-                      for triangle_id, (c, r, w, h) in bounding_rectangles.iteritems()}
+    triangle_colors = map(lambda (c, r, w, h): cv2.mean(im[r:(r + h), c:(c + w), :]), bounding_rectangles)
 
     im_low_poly = np.zeros(im.shape)
-    map(lambda (triangle_id, color_mu):
-        cv2.fillConvexPoly(im_low_poly, triangle_vertices[triangle_id], color_mu),
-        triangle_means.iteritems())
+    map(lambda (vertices, color): cv2.fillConvexPoly(im_low_poly, vertices, color),
+        zip(triangle_vertices, triangle_colors))
 
     return im_low_poly.astype(np.uint8)
-
-
-def im_close(im, n=5):
-    im = cv2.dilate(im, np.ones((n, n), np.uint8), iterations=1)
-    im = cv2.erode(im, np.ones((n, n), np.uint8), iterations=1)
-    return im
-
-
-def im_open(im, n=5):
-    im = cv2.erode(im, np.ones((n, n), np.uint8), iterations=1)
-    im = cv2.dilate(im, np.ones((n, n), np.uint8), iterations=1)
-    return im
-
-
-def threshold_image(im, color_mu, color_std=0, sigma=1):
-    nim = im.copy()
-    _, ima = cv2.threshold(im, color_mu - (color_std * sigma), 255, cv2.THRESH_BINARY)
-    _, imb = cv2.threshold(im, color_mu + (color_std * sigma), 255, cv2.THRESH_BINARY_INV)
-    nim[np.equal(ima, imb)] = 255
-    nim[np.not_equal(ima, imb)] = 0
-    nim = im_open(nim)
-    nim = im_open(nim)
-    return nim
-
-
-def im_mask(im):
-    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    card_color_mu, card_color_std = cv2.meanStdDev(im_gray)
-    return threshold_image(im_gray, card_color_mu, card_color_std).astype(np.uint8)
 
 
 # auto canny function
@@ -68,79 +31,49 @@ def auto_canny(im, sigma=0.33):
     return cv2.Canny(im, lower, upper)
 
 
-def get_triangulation(im, debug=False):
-    if debug:
-        def nothing(x):
-            pass
-        cv2.namedWindow("c")
-        cv2.createTrackbar("Epsilon Factor", "c", 0, 100, nothing)
-        cv2.createTrackbar("Edge Sigma", "c", 0, 100, nothing)
+def get_triangulation(im, e_factor=0.33, sigma=0.33):
 
-    edges = auto_canny(im)
+    edges = auto_canny(im, sigma)
     contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = [contour for contour in contours if 2 < cv2.contourArea(contour)]
-    epsilons = map(lambda x: 1 * cv2.arcLength(x, True) / 100.0, contours)
+    epsilons = map(lambda x: e_factor * cv2.arcLength(x, True) / 100.0, contours)
     contours = map(lambda (contour, epsilon): cv2.approxPolyDP(contour, epsilon, True), zip(contours, epsilons))
     pts = np.vstack(map(lambda c: np.array(c).reshape((-1, 2)), contours))
     # now add the four corners, to get better results at corners
-    sz = im.shape
-    rMax = sz[0]
-    cMax = sz[1]
-    pts = np.vstack([pts, [0, 0]])
-    pts = np.vstack([pts, [0, rMax]])
-    pts = np.vstack([pts, [cMax, 0]])
-    pts = np.vstack([pts, [cMax, rMax]])
-
-    while debug:
-        e_factor = cv2.getTrackbarPos("Epsilon Factor", "c") / 100.0
-        sigma = cv2.getTrackbarPos("Edge Sigma", "c") / 100.0
-        edges = auto_canny(im, sigma)
-        contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        contours = [contour for contour in contours if 2 < cv2.contourArea(contour)]
-        epsilons = map(lambda x: e_factor * cv2.arcLength(x, True), contours)
-        contours = map(lambda (contour, epsilon): cv2.approxPolyDP(contour, epsilon, True), zip(contours, epsilons))
-        pts = np.vstack(map(lambda c: np.array(c).reshape((-1, 2)), contours))
-        contour_image = np.zeros(im.shape)
-        cv2.drawContours(contour_image, contours, -1, (255, 0, 0))
-        cv2.imshow('c', contour_image)
-        k = cv2.waitKey(1) & 0xFF
-        if k == 27:
-            break
+    # note that contours is in [x, y] NOT [r, c], so when adding corners, we also need to flip the r/c
+    row_max, col_max, channels = im.shape
+    corners = np.vstack(([0, 0], [0, row_max], [col_max, 0], [col_max, row_max]))
+    pts = np.vstack((pts, corners))
 
     tris = Delaunay(pts)
 
     return tris
 
 
-def preProcess(hipo, newSize=None):
-    # handle gray scale
-    if hipo.shape[2] == 1:
-        hipo = hipo.dstack([hipo, hipo, hipo], axis=2)
-    if newSize is not None:
-        scale = newSize / float(np.max(hipo.shape[:2]))
-        hipo = cv2.resize(hipo, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
-    return hipo
+def wrapper(in_name, out_name=None, show=False, new_size=None, debug=False):
 
+    oim = cv2.imread(in_name, flags=cv2.CV_LOAD_IMAGE_COLOR)  # ensure we read it in as a color image
+    if new_size is not None:
+        scale = new_size / float(np.max(oim.shape[:2]))
+        oim = cv2.resize(oim, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
 
-def wrapper(inName, outName=None, show=False):
-    hipo = cv2.imread(inName)
-
-    hipo = preProcess(hipo, newSize=500)
+    e_factor = 0.01
+    sigma = 0.33
 
     t = time.time()
-    tris = get_triangulation(hipo, debug=False)
-    lopo = getLowPoly(tris, hipo)
-    print 'Time:',time.time() - t
+    triangle_data = get_triangulation(oim, e_factor, sigma)
+    im_low_poly = color_triangles(triangle_data, oim)
+    print 'Time:', time.time() - t
+
     if show:
-        compare = np.hstack([hipo, lopo])
+        cv2.namedWindow('Compare',flags=cv2.WINDOW_NORMAL)
+        compare = np.hstack([oim, im_low_poly])
         cv2.imshow('Compare', compare)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-    if outName is not None:
-        cv2.imwrite(outName, lopo)
+    if out_name is not None:
+        cv2.imwrite(out_name, im_low_poly)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) < 2:
         print 'Usage'
         print 'lowPoly.py <infile> [outfile]'
@@ -152,4 +85,4 @@ if __name__ == "__main__":
         outFile = None
         if len(sys.argv) == 3:
             outFile = sys.argv[2]
-        wrapper(inName=inFile, outName=outFile, show=True)
+        wrapper(in_name=inFile, out_name=outFile, show=True, new_size=None, debug=True)
