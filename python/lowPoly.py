59ee0044a9c31f80e3c3432bd86cf5438a5e84a7
= <billtholomew@gmail.com>
2016-11-05 00:31:31 -0400

    Resizing feature removed, epsilon hard-coded at 0.001

--- python/lowPoly.py
+++ python/lowPoly.py
@@ -5,16 +5,19 @@ import numpy as np
 from scipy.spatial import Delaunay
 
 
-# create a new image using the average color for each triangle
 def color_triangles(tris, im):
-
+    # 0.123 s
     triangle_vertices = map(lambda vertices: np.array([vertices]), tris.points[tris.vertices].astype(np.int32))
 
+    # 0.035 s
     bounding_rectangles = map(lambda vertices: cv2.boundingRect(vertices), triangle_vertices)
 
+    # 0.123 s
     triangle_colors = map(lambda (c, r, w, h): cv2.mean(im[r:(r + h), c:(c + w), :]), bounding_rectangles)
 
     im_low_poly = np.zeros(im.shape)
+
+    # 0.067 s
     map(lambda (vertices, color): cv2.fillConvexPoly(im_low_poly, vertices, color),
         zip(triangle_vertices, triangle_colors))
 
@@ -31,11 +34,10 @@ def auto_canny(im, sigma=0.33):
     return cv2.Canny(im, lower, upper)
 
 
-def get_triangulation(im, e_factor=0.33, sigma=0.33):
-
+def get_triangulation(im, sigma=0.33):
     edges = auto_canny(im, sigma)
     contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    epsilons = map(lambda x: e_factor * cv2.arcLength(x, True) / 100.0, contours)
+    epsilons = map(lambda x: 0.001 * cv2.arcLength(x, True), contours)
     contours = map(lambda (contour, epsilon): cv2.approxPolyDP(contour, epsilon, True), zip(contours, epsilons))
     pts = np.vstack(map(lambda c: np.array(c).reshape((-1, 2)), contours))
     # now add the four corners, to get better results at corners
@@ -44,28 +46,25 @@ def get_triangulation(im, e_factor=0.33, sigma=0.33):
     corners = np.vstack(([0, 0], [0, row_max], [col_max, 0], [col_max, row_max]))
     pts = np.vstack((pts, corners))
 
+    tt = time.time()
     tris = Delaunay(pts)
-
+    print time.time() - tt
     return tris
 
 
-def wrapper(in_name, out_name=None, show=False, new_size=None, debug=False):
+def wrapper(in_name, out_name=None, show=False):
 
     oim = cv2.imread(in_name, flags=cv2.CV_LOAD_IMAGE_COLOR)  # ensure we read it in as a color image
-    if new_size is not None:
-        scale = new_size / float(np.max(oim.shape[:2]))
-        oim = cv2.resize(oim, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
-
-    e_factor = 0.01
-    sigma = 0.33
 
     t = time.time()
-    triangle_data = get_triangulation(oim, e_factor, sigma)
+    sigma = 0.33
+    triangle_data = get_triangulation(oim, sigma)
     im_low_poly = color_triangles(triangle_data, oim)
-    print 'Time:', time.time() - t
+    print 'Total:', time.time() - t
+
 
     if show:
-        cv2.namedWindow('Compare',flags=cv2.WINDOW_NORMAL)
+        cv2.namedWindow('Compare', flags=cv2.WINDOW_NORMAL)
         compare = np.hstack([oim, im_low_poly])
         cv2.imshow('Compare', compare)
         cv2.waitKey(0)
@@ -85,4 +84,4 @@ if __name__ == '__main__':
         outFile = None
         if len(sys.argv) == 3:
             outFile = sys.argv[2]
-        wrapper(in_name=inFile, out_name=outFile, show=True, new_size=None, debug=True)
+        wrapper(in_name=inFile, out_name=outFile, show=True)
